06496a57fabbd2fc51ec60f948cb41d19fac60ba
YangJie <yangjie6759@163.com>
2024-10-15 10:25:45 +0800

    feat: update

---
+++ ArduinoCore-API/test/src/dtostrf.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Arduino.  All rights reserved.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <api/deprecated-avr-comp/avr/dtostrf.h>
+
+#include <stdlib.h>
+
+/**************************************************************************************
+ * FUNCTION IMPLEMENTATION
+ **************************************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <api/deprecated-avr-comp/avr/dtostrf.c.impl>
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
